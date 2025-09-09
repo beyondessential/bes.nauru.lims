bf5ebb1f23889b1d15ec597fc5bf43684ae7a986
Jordi Puiggené <jp@naralabs.com>
2025-09-09 14:45:03 +0200

    Add senaite.storage as a dependency

--- setup.py
+++ setup.py
@@ -53,12 +53,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "bes.lims>=1.0.0",
-        # senaite.core does no longer provides schemaextender
-        # https://github.com/senaite/senaite.core/pull/1931
-        "archetypes.schemaextender",
-        # Python 2.7: python-slugify < 5.0.0
-        # Python 3.6+: python-slugify >= 5.0.0
-        "python-slugify < 5.0.0",
+        "senaite.storage>=2.6.0",
     ],
     extras_require={
         "test": [
